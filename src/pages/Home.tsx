f33fbb838ce91d7b7661f33fa48dae36a1de97cc
matmagamb <ceifa13@outlook.com>
2022-02-14 14:05:47 -0300

    toggle

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,6 +20,13 @@ export function Home() {
   }
 
   function handleToggleTaskDone(id: number) {
+    const updatedTasks = tasks.map(task => ({ ...task })); 
+    const foundItem = updatedTasks.find(task => task.id === id);
+
+    if(!foundItem)
+    return;
+    foundItem.done =!foundItem.done
+    setTasks(updatedTasks);
     //TODO - toggle task done if exists
   }
 
